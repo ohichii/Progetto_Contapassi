e8ba235f19aa0d11644ca606782c786cd55b51aa
dinco90 <d.incollu@tiscali.it>
2016-02-13 13:09:53 +0100

    aggiunta button in quarta activity per test JSON
    non funzionante al 100%

--- app/src/main/java/com/example/zineb/progetto_contapassi/Fourth.java
+++ app/src/main/java/com/example/zineb/progetto_contapassi/Fourth.java
@@ -1,12 +1,15 @@
 package com.example.zineb.progetto_contapassi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.jar.Attributes;
 
-public class Fourth extends Activity {
+public class Fourth extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,21 @@ public class Fourth extends Activity {
 
         text.setText(nom);
 
+        // test JSON
+        Button bnt = (Button) findViewById(R.id.buttonTestJSON);
+        bnt.setOnClickListener(this);
+        // test JSON
+
     }
 
+    // test JSON
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonTestJSON:
+                Intent i = new Intent (this, SaveJSON.class);
+                startActivity(i);
+        }
+    }
+    // test JSON
 }

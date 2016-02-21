a8d6867f08910266f43fecf3eac638fdf9ae5628
Sergiod3x <sergiod3x@gmail.com>
2016-02-21 16:04:33 +0100

    Auto cambio activity

--- app/src/main/java/com/example/zineb/progetto_contapassi/MainActivity.java
+++ app/src/main/java/com/example/zineb/progetto_contapassi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.zineb.progetto_contapassi;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,23 +13,29 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import java.lang.Runnable;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements Runnable {
 
     static DataBaseHelper myDb;
+    //private final static String REGISTRATION_PREFERENCES = "RegPref";
+   // private final static String REGISTRATION_DATA = "RegDat";
+    //verifico che la registrazione sia già stata effettuata
+    //SharedPreferences prefs = getSharedPreferences(REGISTRATION_PREFERENCES, Context.MODE_PRIVATE);
+   // boolean a = prefs.getBoolean(REGISTRATION_DATA, true);
+    boolean b = true;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bnt = (Button) findViewById(R.id.buttonWelc);
-        bnt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Second.class);
-                startActivity(intent);
-            }
-        });
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(this, 2500);
 
         myDb = new DataBaseHelper(this);
     }
@@ -51,6 +60,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void run ()
+    {
+        if (b) {    //se la prima registrazione è stata effetuata passo direttamente al contappassi
+            Intent intent = new Intent(this, Second.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(this, CoreActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 }
